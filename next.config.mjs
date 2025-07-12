ebe5264718a795076bdb7dbc069ee59ebbd77e80
George Bobby <georgebobbyawesome@gmail.com>
2025-07-12 20:34:36 +0530

    remove sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,202 +1,141 @@
-import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  customWorkerDir: 'worker',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts',
-        expiration: {
-          maxEntries: 20,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-font-assets',
-        expiration: {
-          maxEntries: 20,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-image-assets',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/image\?url=.+$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'next-image',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:mp3|wav|ogg)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-audio-assets',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:mp4|webm)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-video-assets',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-js-assets',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:css|less)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-style-assets',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'next-data',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60, // 1 hour
-        },
-      },
-    },
-    {
-      urlPattern: /\/api\/.*$/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'apis',
-        expiration: {
-          maxEntries: 16,
-          maxAgeSeconds: 60 * 60, // 1 hour
-        },
-        networkTimeoutSeconds: 10, // fall back to cache if api doesn't respond within 10 seconds
-      },
-    },
-    {
-      urlPattern: /.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'others',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60, // 1 hour
-        },
-        networkTimeoutSeconds: 10,
-      },
-    },
-  ],
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === 'development',
+	customWorkerDir: 'worker',
+	runtimeCaching: [
+		{
+			urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
+			handler: 'CacheFirst',
+			options: {
+				cacheName: 'google-fonts',
+				expiration: {
+					maxEntries: 20,
+					maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+			handler: 'StaleWhileRevalidate',
+			options: {
+				cacheName: 'static-font-assets',
+				expiration: {
+					maxEntries: 20,
+					maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+			handler: 'StaleWhileRevalidate',
+			options: {
+				cacheName: 'static-image-assets',
+				expiration: {
+					maxEntries: 64,
+					maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+				},
+			},
+		},
+		{
+			urlPattern: /\/_next\/image\?url=.+$/i,
+			handler: 'StaleWhileRevalidate',
+			options: {
+				cacheName: 'next-image',
+				expiration: {
+					maxEntries: 64,
+					maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:mp3|wav|ogg)$/i,
+			handler: 'CacheFirst',
+			options: {
+				cacheName: 'static-audio-assets',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:mp4|webm)$/i,
+			handler: 'CacheFirst',
+			options: {
+				cacheName: 'static-video-assets',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:js)$/i,
+			handler: 'StaleWhileRevalidate',
+			options: {
+				cacheName: 'static-js-assets',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:css|less)$/i,
+			handler: 'StaleWhileRevalidate',
+			options: {
+				cacheName: 'static-style-assets',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+				},
+			},
+		},
+		{
+			urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'next-data',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60, // 1 hour
+				},
+			},
+		},
+		{
+			urlPattern: /\/api\/.*$/i,
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'apis',
+				expiration: {
+					maxEntries: 16,
+					maxAgeSeconds: 60 * 60, // 1 hour
+				},
+				networkTimeoutSeconds: 10, // fall back to cache if api doesn't respond within 10 seconds
+			},
+		},
+		{
+			urlPattern: /.*/i,
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'others',
+				expiration: {
+					maxEntries: 32,
+					maxAgeSeconds: 60 * 60, // 1 hour
+				},
+				networkTimeoutSeconds: 10,
+			},
+		},
+	],
 })({
-  images: {
-    domains: ['getstream.io'],
-  },
+	images: {
+		domains: ['getstream.io'],
+	},
 });
 
-export default withSentryConfig(withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "personal-hky",
-project: "proddy-monitoring",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-}), {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "personal-hky",
-project: "proddy-monitoring",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+export default nextConfig;
